1d7a4f9f80719f783e69dfbdd7967bec0bc59207
Shawn Catanzarite <me@shawncatz.com>
2020-05-09 21:56:37 -0700

    fix test app

--- test/models/connector.go
+++ test/models/connector.go
@@ -8,15 +8,15 @@ type Connector struct {
 }
 
 func NewConnector() (*Connector, error) {
-    download, err := NewDownloadStore("mongodb://localhost:27017", "database", "download")
+    download, err := NewDownloadStore("mongodb://localhost:27017", "seer_production", "downloads")
     if err != nil {
         return nil, err
     }
-    medium, err := NewMediumStore("mongodb://localhost:27017", "database", "medium")
+    medium, err := NewMediumStore("mongodb://localhost:27017", "seer_production", "media")
     if err != nil {
         return nil, err
     }
-    release, err := NewReleaseStore("mongodb://localhost:27017", "database", "release")
+    release, err := NewReleaseStore("mongodb://localhost:27017", "torch_production", "torrents")
     if err != nil {
         return nil, err
     }
